7410e4b3fe46e1b6bb6aa0c9936c76c469f412b7
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-19 21:20:56 -0300

    flutter

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -18,21 +18,73 @@ class MealItem extends StatelessWidget {
         margin: const EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
-            Stack(children: <Widget>[
-              ClipRRect(
-                // define que o widget fique com as bordas
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  // define que o widget fique com as bordas
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Image.network(
-                  meal.imageUrl,
-                  height: 250,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.white70,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.teal[600],
+                      ),
+                      softWrap: true, //caso tenha quebra de linha, permite
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 ),
-              )
-            ]),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('${meal.duration} min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text('${meal.complexityText}'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text('${meal.costText}'),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
